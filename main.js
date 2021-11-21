df0c86949c254abd7155dcd4c5a9b7977d06a7c3
totielabrie11 <martinezcarlosmanuel.1@gmail.com>
2021-11-21 13:12:49 -0300

    agrego funcion que detecta evento de cada una de las cards

--- main.js
+++ main.js
@@ -42,7 +42,71 @@ document.addEventListener('DOMContentLoaded', function() {
 });
     
 
-$( "#carousel" ).on( "click", function() {
-  console.log( $('#totie').val() );
-});
+//funcion para levantar el evento de cada cards y ejecute una funcion para cada una
+function pintarDomJazz() {
+  console.log("estoy entrando en la funcion que pinta el calendario en Jazz")
+}
+function pintarDomRegeton() {
+  console.log("estoy entrando en la funcion que pinta el calendario en regeton")
+}
+function pintarDomKpop() {
+  console.log("estoy entrando en la funcion que pinta el calendario en Kpop")
+}
+function pintarDomRock() {
+  console.log("estoy entrando en la funcion que pinta el calendario en rock")
+}
+function pintarDomKickBM() {
+  console.log("estoy entrando en la funcion que pinta el calendario en kickBoxing")
+}
+function pintarDomKickBF() {
+  console.log("estoy entrando en la funcion que pinta el calendario en kickBoxingFemm")
+}
+function pintarDomTaeK() {
+  console.log("estoy entrando en la funcion que pinta el calendario en Tae Kwondo")
+}
+function pintarDomLocalizada() {
+  console.log("estoy entrando en la funcion que pinta el calendario en localizada")
+}
+function pintarDomAerobox() {
+  console.log("estoy entrando en la funcion que pinta el calendario en AeroBox")
+}
+function pintarDomZumba() {
+  console.log("estoy entrando en la funcion que pinta el calendario en zumba")
+}
+function pintarDomYoga() {
+  console.log("estoy entrando en la funcion que pinta el calendario en yoga")
+}
+function pintarDomVisuales() {
+  console.log("estoy entrando en la funcion que pinta el calendario en artes visuales")
+}
+
+window.onload = function(){
+  const cardJ = document.getElementById("jazz");
+  const cardR = document.getElementById("regeton");
+  const cardK = document.getElementById("kpop");
+  const cardRock = document.getElementById("Rock");
+  const cardKBM = document.getElementById("KickBM");
+  const cardKBF = document.getElementById("KickBF");
+  const cardTK = document.getElementById("TaeK");
+  const localizada = document.getElementById("localizada");
+  const aerobox = document.getElementById("aerobox");
+  const zumba = document.getElementById("zumba");
+  const yoga = document.getElementById("yoga");
+  const visuales = document.getElementById("visuales");
+
+  cardJ.onclick = pintarDomJazz
+  cardR.onclick = pintarDomRegeton
+  cardK.onclick = pintarDomKpop
+  cardRock.onclick = pintarDomRock
+  cardKBM.onclick = pintarDomKickBM
+  cardKBF.onclick = pintarDomKickBF
+  cardTK.onclick = pintarDomTaeK
+  localizada.onclick = pintarDomLocalizada
+  aerobox.onclick = pintarDomAerobox
+  zumba.onclick = pintarDomZumba
+  yoga.onclick = pintarDomYoga
+  visuales.onclick = pintarDomVisuales
+}
+
+
 
